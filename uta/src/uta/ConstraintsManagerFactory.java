3169e71f67d5352daef5ed8f9b77e4a7af39a649
lukasz.golebiewski <lukasz.golebiewski@gmail.com>
2010-09-25 13:15:50 +0000

    ConstantKendallConstraintsManager added to factory

--- uta/src/uta/ConstraintsManagerFactory.java
+++ uta/src/uta/ConstraintsManagerFactory.java
@@ -2,39 +2,7 @@ package uta;
 
 public class ConstraintsManagerFactory {
 
-	private class DummyConstraintsManager implements ConstraintsManager {
-
-		public DummyConstraintsManager() {
-		}
-
-		@Override
-		public void update(LinearFunction function, Point changedPoint) {
-
-		}
-
-		@Override
-		public Constraint[] getConstraints() {
-			return new Constraint[0];
-		}
-
-		@Override
-		public Constraint getConstraintFor(Point p) {
-			return null;
-		}
-
-	}
-
-	private static final DummyConstraintsManager DUMMY_MANAGER;
-
-	static {
-		DUMMY_MANAGER = new ConstraintsManagerFactory().new DummyConstraintsManager();
-	}
-
 	private boolean freezeKendall;
-
-	private ConstraintsManagerFactory() {
-
-	}
 
 	public ConstraintsManagerFactory(boolean freezeKendall) {
 		this.setFreezeKendall(freezeKendall);
@@ -44,8 +12,8 @@ public class ConstraintsManagerFactory {
 			Ranking<Alternative> finalRanking) {
 		if (!freezeKendall)
 			return new StandardConstraintsManager(functions);
-		// TODO return KendallAwareConstraintsManager
-		return DUMMY_MANAGER;
+
+		return new ConstantKendallConstraintsManager(functions, referenceRanking, finalRanking);
 	}
 
 	public void setFreezeKendall(boolean freezeKendall) {
