4a804fd558c09ce5a2932c913a9decf7958da344
lukasz.golebiewski <lukasz.golebiewski@gmail.com>
2010-11-03 18:28:21 +0000

    UtaFX-test project initial import

---
+++ UtaFX-test/src/utafx/RunAppTest.java
@@ -0,0 +1,33 @@
+package utafx;
+
+import java.io.InputStreamReader;
+import java.util.Date;
+
+import javax.script.Bindings;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.SimpleScriptContext;
+
+public class RunAppTest {
+
+	public static void main(String[] args) throws Exception {
+		// set up script:
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		ScriptEngineManager manager = new ScriptEngineManager(loader);
+		ScriptEngine engine = manager.getEngineByExtension("fx");
+
+		Bindings bindings = engine.createBindings();
+		bindings.put("now:java.util.Date", new Date()); // Note-1
+
+		ScriptContext context = new SimpleScriptContext();
+		// Bug workaround
+		context.setBindings(bindings, ScriptContext.GLOBAL_SCOPE);
+		context.setBindings(bindings, ScriptContext.ENGINE_SCOPE);
+		engine.setContext(context);
+
+		InputStreamReader reader = new InputStreamReader(RunAppTest.class.getResourceAsStream("HelloWorld.fx"));
+		Object scriptReturnValue = engine.eval(reader);
+		System.out.println(scriptReturnValue);
+	}
+}
