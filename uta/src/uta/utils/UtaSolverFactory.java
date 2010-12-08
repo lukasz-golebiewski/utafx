46d18f89d711128d5141570f92fcd90eae97fa01
lukasz.golebiewski <lukasz.golebiewski@gmail.com>
2010-12-08 22:01:20 +0000

    User defined UTA implementation. -- Part 1 : Solver factory introduced

--- uta/src/uta/utils/UtaSolverFactory.java
+++ uta/src/uta/utils/UtaSolverFactory.java
@@ -1,12 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package uta.utils;
 
+import java.util.Iterator;
+import java.util.ServiceLoader;
+import java.util.logging.Logger;
+
 import uta.api.IUtaSolver;
-import uta.star.UtaStarSolver;
 
 /**
  * 
@@ -14,8 +12,30 @@ import uta.star.UtaStarSolver;
  */
 public class UtaSolverFactory {
 
-    public IUtaSolver createSolver() {
-        // stub:
-        return new UtaStarSolver();
-    }
+	private static final Logger LOG = Logger.getLogger(UtaSolverFactory.class
+			.getName());
+
+	private static final String FOUND_SOLVER = "Found solver: ";
+
+	private static final String CHOOSING_SOLVER = "Will solve using the following solver class: ";
+
+	private static final String DUMMY_JAR_MISSING = "Couldn't load the default solver - make sure uta-dummy.jar is on the runtime classpath";
+	
+	public IUtaSolver createSolver() {
+		ServiceLoader<IUtaSolver> serviceLoader = ServiceLoader
+				.load(IUtaSolver.class);
+
+		Iterator<IUtaSolver> loadedServicesIterator = serviceLoader.iterator();
+		if (!loadedServicesIterator.hasNext()) {
+			throw new RuntimeException(DUMMY_JAR_MISSING);
+		}
+
+		IUtaSolver solver = null;
+		while (loadedServicesIterator.hasNext()) {
+			solver = loadedServicesIterator.next();
+			LOG.info(FOUND_SOLVER + solver.getClass().getName());
+		}
+		LOG.info(CHOOSING_SOLVER + solver.getClass().getName());
+		return solver;
+	}
 }
