5951fec7a75a020fd4c3544ef0ee1fb0e1dfe2a6
lukasz.golebiewski <lukasz.golebiewski@gmail.com>
2010-12-12 21:21:28 +0000

    New features: - browser and webstart launch configurations added - solver factory returns default solver implementation in case of errors (such as access restrictions or if a jar file without solver implementation was chosen)

--- uta/src/uta/utils/UtaSolverFactory.java
+++ uta/src/uta/utils/UtaSolverFactory.java
@@ -9,6 +9,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import uta.api.IUtaSolver;
+import uta.star.UtaStarSolver;
 
 /**
  * 
@@ -23,32 +24,35 @@ public class UtaSolverFactory {
 
 	private static final String CHOOSING_SOLVER = "Will solve using the following solver class: ";
 
-	private static final String DUMMY_JAR_MISSING = "Couldn't load the default solver - make sure uta-default.jar is on the runtime classpath";
-
 	private String solverJarPath = "";
 
 	public IUtaSolver createSolver() {
-		URL[] urls = new URL[1];
-		try {
-			urls[0] = new URL("file://" + solverJarPath);
-		} catch (MalformedURLException ex) {
-			Logger.getLogger(UtaSolverFactory.class.getName()).log(
-					Level.SEVERE, null, ex);
-		}
-
-		ServiceLoader<IUtaSolver> serviceLoader = ServiceLoader.load(
-				IUtaSolver.class, new URLClassLoader(urls));
-
-		Iterator<IUtaSolver> loadedServicesIterator = serviceLoader.iterator();
-		if (!loadedServicesIterator.hasNext()) {
-			throw new RuntimeException(DUMMY_JAR_MISSING);
-		}
-
 		IUtaSolver solver = null;
-		while (loadedServicesIterator.hasNext()) {
-			solver = loadedServicesIterator.next();
-			LOG.info(FOUND_SOLVER + solver.getClass().getName());
+
+		try {
+			URL[] urls = new URL[1];
+			urls[0] = new URL("file://" + solverJarPath);
+
+			ServiceLoader<IUtaSolver> serviceLoader = ServiceLoader.load(
+					IUtaSolver.class, new URLClassLoader(urls));
+
+			Iterator<IUtaSolver> loadedServicesIterator = serviceLoader
+					.iterator();			
+
+			if (loadedServicesIterator.hasNext()) {
+				solver = loadedServicesIterator.next();
+				LOG.info(FOUND_SOLVER + solver.getClass().getName());
+			}
+		} catch (java.security.AccessControlException e) {
+			LOG.log(Level.INFO, null, e);
+		} catch (MalformedURLException e) {
+			LOG.log(Level.SEVERE, null, e);
+		} finally {
+			if (solver == null) {
+				solver = new UtaStarSolver();
+			}
 		}
+
 		LOG.info(CHOOSING_SOLVER + solver.getClass().getName());
 		return solver;
 	}
